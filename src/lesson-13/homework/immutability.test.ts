bb494b68c2a1d3654c5a8ced98dc3871141313a9
Чернецова Татьяна <chernetsova.tatya@gmail.com>
2020-05-28 01:46:38 +0300

    immutability

--- src/lesson-13/homework/immutability.test.ts
+++ src/lesson-13/homework/immutability.test.ts
@@ -1,48 +1,48 @@
-import { OriginalTeam, ExpectedTeam } from "./immutability";
+import { OriginalTeam, ExpectedTeam, originalTeamToExpectedTeam, originalArrayToExpectedArray, deepOriginalTeamToExpectedTeam } from "./immutability";
 
 // Задание 1
 test("team to team", () => {
-  const originalTeam: OriginalTeam = Object.freeze({
-    size: 15,
-    name: "Tampa Bay Roosters",
-    league: "Minor",
-  });
+	const originalTeam: OriginalTeam = Object.freeze({
+		size: 15,
+		name: "Tampa Bay Roosters",
+		league: "Minor",
+	});
 
-  const expectedTeam: ExpectedTeam = {
-    name: "New York Badgers",
-    league: "Minor",
-    roster: 25,
-  };
+	const expectedTeam: ExpectedTeam = {
+		name: "New York Badgers",
+		league: "Minor",
+		roster: 25,
+	};
 
-  expect(originalTeamToExpectedTeam(originalTeam)).toBe(expectedTeam);
+	expect(originalTeamToExpectedTeam(originalTeam)).toEqual(expectedTeam);
 });
 
 // Задание 2
 test("array to array", () => {
-  const originalArray = Object.freeze([1, 2, 3, 4]);
+	const originalArray = Object.freeze([1, 2, 3, 4]);
 
-  const expectedArray = ["two", 3, 4, 5];
+	const expectedArray = ["two", 3, 4, 5];
 
-  expect(originalArrayToExpectedArray(originalArray)).toBe(expectedArray);
+	expect(originalArrayToExpectedArray(originalArray)).toEqual(expectedArray);
 });
 
 // Задание 3
 test("team to team deep", () => {
-  const originalTeam = Object.freeze({
-    name: "Tampa Bay Roosters",
-    captain: {
-      name: "Bryan Downey",
-      age: 27,
-    },
-  });
+	const originalTeam = Object.freeze({
+		name: "Tampa Bay Roosters",
+		captain: {
+			name: "Bryan Downey",
+			age: 27,
+		},
+	});
 
-  const expectedTeam = {
-    name: "Tampa Bay Roosters",
-    captain: {
-      name: "Bryan Downey",
-      age: 28,
-    },
-  };
+	const expectedTeam = {
+		name: "Tampa Bay Roosters",
+		captain: {
+			name: "Bryan Downey",
+			age: 28,
+		},
+	};
 
-  expect(originalTeamToExpectedTeam(originalTeam)).toBe(expectedTeam);
+	expect(deepOriginalTeamToExpectedTeam(originalTeam)).toEqual(expectedTeam);
 });
